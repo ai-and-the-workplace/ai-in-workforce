1c5513b17d1c9ace96c49569667a53e96d9c4132
Andrew Qiao <andrewqiao2004@gmail.com>
2023-03-26 22:37:27 -0400

    Change model to gpt-3.5-turbo

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from 'openai';
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,12 +8,14 @@ const openai = new OpenAIApi(configuration);
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req, res) {
-  const completion = await openai.createCompletion({
-    model: 'text-davinci-002',
-    prompt: req.body.prompt,
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: req.body.prompt }],
     temperature: 0.7,
     max_tokens: 1500,
   });
 
-  res.status(200).json({ response: completion.data.choices[0].text });
+  res
+    .status(200)
+    .json({ response: completion.data.choices[0].message.content });
 }
