00c0142569a42dba4855deb724cc4d2852cfc2db
Andrew Qiao <andrewqiao2004@gmail.com>
2023-03-29 12:03:43 -0400

    Add participant ID to attitudes pages

--- components/screens/Attitudes2.jsx
+++ components/screens/Attitudes2.jsx
@@ -9,8 +9,10 @@ export default function Attitudes2() {
     <div className="m-horizontal">
       <h1 className="title mb-6">Attitudes</h1>
       <p className="body mb-6 leading-loose md:mb-9">
-        Please complete these two questions. Do not press continue until
-        clicking on done in SurveyMonkey.
+        Please complete these two questions. Please wait for the survey to pop
+        up as it takes a few seconds. Your participant ID is:{' '}
+        <strong>{progressContext.progress.id}</strong>. Please enter it into the
+        survey. Do not press continue until clicking on done in SurveyMonkey.
       </p>
       <div className="mb-16 max-w-full">
         <iframe
